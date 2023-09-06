d89bcbb3ce6af3668d8808e6de8ecbc39a0c17f5
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-09-05 22:41:26 -0300

    Lincando o produtos.

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom"
+import { ListaProdutos } from "../../components/listaProduto"
+
 export default function Produtos(){
 
     return(
         <main>
             <h1>Produtos</h1>
+            {ListaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={'/produtos/editar/${prod.id}'}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
-    )
+)
 }
